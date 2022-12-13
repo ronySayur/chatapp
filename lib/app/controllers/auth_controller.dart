5c4904b72f120f52ddc78984e5e97e98ffce99bd
RizkyPratama <rizkypratama218@gmail.com>
2022-12-14 00:06:52 +0700

    auto_login

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import '../routes/app_pages.dart';
@@ -11,6 +12,43 @@ class AuthController extends GetxController {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
   UserCredential? userCredential;
+
+  Future<void> firstInitialized() async {
+    await autoLogin().then((value) {
+      if (value) {
+        isAuth.value = true;
+      }
+    });
+
+    //mengubah isSkip => true
+    await skipIntro().then((value) {
+      if (value) {
+        isSkipIntro.value = true;
+      }
+    });
+  }
+
+  Future<bool> skipIntro() async {
+    final box = GetStorage();
+
+    if (box.read('skipIntro') != null || box.read('skipIntro') == true) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> autoLogin() async {
+    //mengubah isAuth => true => autologin
+    try {
+      final isSignIn = await _googleSignIn.isSignedIn();
+      if (isSignIn) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
 
   Future<void> login() async {
     try {
@@ -37,6 +75,14 @@ class AuthController extends GetxController {
             .signInWithCredential(credential)
             .then((value) => userCredential = value);
 
+        //Simpan status user bahwa pernah login dan tidak akan menampilkan intro screen
+        final box = GetStorage();
+        if (box.read('skipIntro') != null) {
+          box.remove('skipIntro');
+        }
+        //tulis skip intro 'true' pada mesin
+        box.write('skipIntro', true);
+
         isAuth.value = true;
         Get.offAllNamed(Routes.HOME);
       } else {}
@@ -46,6 +92,7 @@ class AuthController extends GetxController {
   }
 
   Future<void> logout() async {
+    await _googleSignIn.disconnect();
     await _googleSignIn.signOut();
     Get.offAllNamed(Routes.LOGIN);
   }
--- lib/utils/splash_screen.dart
+++ lib/utils/splash_screen.dart
@@ -1,19 +1,18 @@
+import 'package:chatapp/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: SizedBox(
-            width: Get.width * 0.7,
-            height: Get.width * 0.7,
+          child: Container(
+            width: wDimension.widthSetengah,
+            height: wDimension.heightSetengah,
             child: Lottie.asset("assets/lottie/hello.json"),
           ),
         ),
