d1d614fe31407f810b3097cdbfb52ff05c37ee3a
RizkyPratama <rizkypratama218@gmail.com>
2022-12-13 20:51:44 +0700

    config auth signin console detect

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -9,23 +10,36 @@ class AuthController extends GetxController {
 
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
+  UserCredential? userCredential;
 
   Future<void> login() async {
     try {
+      //handle kebocoran data user sebelum login
       await _googleSignIn.signOut();
 
+      //pop up sign-in mendapatkan google account
       await _googleSignIn.signIn().then((value) => _currentUser = value);
 
-      await _googleSignIn.isSignedIn().then((value) {
-        if (value) {
-          //kondisi login berhasil
-          isAuth.value = true;
-          Get.offAllNamed(Routes.HOME);
-        } else {
-          //kondisi login gagal
+      //status login user
+      final isSignIn = await _googleSignIn.isSignedIn();
 
-        }
-      });
+      //cek status login user
+      if (isSignIn) {
+        //kondisi login berhasil
+        final googleAuth = await _currentUser!.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) => userCredential = value);
+
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {}
     } catch (error) {
       print(error);
     }
